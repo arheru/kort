45c200c8b99e24009760473176bb93f7075698e9
Henrik Lysell <trumhenrik@gmail.com>
2015-08-10 14:21:51 +0200

    6stth

--- kort.py
+++ kort.py
@@ -14,6 +14,7 @@ class Deck(object):
         self.name = name
         self.parent = parent
         self.cards = []
+        self.discard_pile = []
 
     def __repr__(self):
         return self.name
@@ -24,12 +25,12 @@ class Deck(object):
 class Card(object):
     """
     """
-    def __init__(self, parent, name, number_of_targets):
+    def __init__(self, parent, name):
         """
         """
         self.parent = parent
         self.name = name
-        self.number_of_targets = number_of_targets
+        self.number_of_targets = 1
 
     def __repr__(self):
         return self.name
@@ -41,18 +42,18 @@ class Card(object):
         pass
 
     def choose_target(self, player):
+        targets = []
         # "All" means all players including card's owner.
         players = self.parent.parent.players
         if self.number_of_targets == 'all':
             targets = players
             return targets
         # "Others" means all players except card's owner.
-        if self.number_of_targets == 'others':
-            targets = []
+        elif self.number_of_targets == 'others':            
             for p in players:
                 if p != player:
                     targets.append(p)
-        targets = []
+            return targets
         # Limited number of targets
         while len(targets) < self.number_of_targets:
             print "This card affects %s players. Who do you want the card to affect?" % self.number_of_targets
@@ -68,7 +69,7 @@ class Card(object):
                 print "Error: Choose a number between 1-%s" % len(players)
                 continue
             if key not in range(len(players)+1):
-                print "Error: Only keys 1-%s are valid options. Try again." % len(players)
+                print "Error: Only keys 1-%s are valname options. Try again." % len(players)
                 continue
             targets.append(players[key-1])
             return targets
@@ -76,16 +77,16 @@ class Card(object):
 class CardUpOne(Card):
     """
     """
-    def __init__(self, parent, name, number_of_targets):
+    def __init__(self, parent, name):
         """
         """
         self.parent = parent
         self.name = name
-        self.number_of_targets = number_of_targets
+        self.number_of_targets = 1
 
     def activate(self, player, targets):
         """
-        Activate Card's effect on target Player.
+        Increase one target player's position by 1.
         """
         target = targets[0]
         old_position = target.position
@@ -95,27 +96,46 @@ class CardUpOne(Card):
 class CardDownOne(Card):
     """
     """
-    def __init__(self, parent, name, number_of_targets):
+    def __init__(self, parent, name):
         """
         """
         self.parent = parent
         self.name = name
-        self.number_of_targets = number_of_targets
+        self.number_of_targets = 1
 
     def activate(self, player, targets):
         """
-        Activate Card's effect on target Player.
+        Decrease one target player's position by 1.
         """
         target = targets[0]
         old_position = target.position
         target.position -= 1
         print "%s's position goes from %s to %s." % (target, old_position, target.position)
 
+class CardAllUpOne(Card):
+    """
+    """
+    def __init__(self, parent, name):
+        """
+        """
+        self.parent = parent
+        self.name = name
+        self.number_of_targets = 'all'
+
+    def activate(self, player, targets):
+        """
+        Increase one target player's position by 1.
+        """
+        for target in targets:
+            old_position = target.position
+            target.position += 1
+            print "%s's position goes from %s to %s." % (target, old_position, target.position)
+
 
 class Player(object):
     """
     """
-    def __init__(self, name, parent, position = 5):
+    def __init__(self, name, parent, position = 3):
         """
         """
         self.name = name
@@ -131,11 +151,15 @@ class Player(object):
         Player draws number amount of Cards from Deck.
         """
         for i in range(number):
-            print deck.cards[-1]
+            # When deck is empty, the entire discard pile is shuffled into the deck.
+            if not deck.cards:
+                deck.cards = deck.discard_pile
+                deck.shuffle()
             print "%s draws %s" % (self, deck.cards[-1])
             self.hand.append(deck.cards.pop())
 
     def discard(self, card):
+        self.parent.decks[0].discard_pile.append(card)
         self.hand.remove(card)
         print "%s discards %s." % (self, card)
 
@@ -188,6 +212,19 @@ class Game(object):
             while True:
                 turn_number += 1
                 apocalypse_position += 1
+
+                # End of game conditions
+                os.system('clear')
+                for player in self.players:
+                    if player.position <= apocalypse_position:
+                        print "%s is engulfed by the apocalypse!" % player
+                        self.graveyard.append(player)
+                        self.players.remove(player)
+                        print "graveyard is now", self.graveyard
+                        continue
+                if len(self.players) <= 0:
+                    print "Everyone died!"
+                    return False
                 print "\n\n\nTurn no %s\t\nCurrent players:" % turn_number
                 for player in self.players:
                     print "%s\t%s\t%s" % (player, player.hand, player.position)
@@ -205,18 +242,6 @@ class Game(object):
                     card.activate(player, target_list)
                     player.discard(card)
 
-                # End of game conditions
-                for player in self.players:
-                    if player.position < apocalypse_position:
-                        print "%s is engulfed by the apocalypse!" % player
-                        self.graveyard.append(player)
-                        self.players.remove(player)
-                        print "graveyard is now", self.graveyard
-                        continue
-                os.system('clear')
-                if len(self.players) <= 0:
-                    print "Everyone died!"
-                    return False
             return False
 
 
@@ -224,13 +249,17 @@ if __name__ == "__main__":
     game = Game()
     deck = Deck('deck1', game)
     game.add_deck(Deck('deck1', game))
-    for i in range(50):
+    for i in range(4):
         card_name = 'up1_0%s' % (i+1)
-        game.decks[0].cards.append(CardUpOne(deck, card_name, 1))
+        game.decks[0].cards.append(CardUpOne(deck, card_name))
 
-    for i in range(28):
+    for i in range(3):
         card_name = 'down1_0%s' % (i+1)
-        game.decks[0].cards.append(CardDownOne(deck, card_name, 1))
+        game.decks[0].cards.append(CardDownOne(deck, card_name))
+
+    for i in range(3):
+        card_name = 'allup1_0%s' % (i+1)
+        game.decks[0].cards.append(CardAllUpOne(deck, card_name))
 
     for i in range(3):
         player_name = 'p%s' % (i+1)
